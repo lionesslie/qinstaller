10b276078431605796236a603308891ea51acac7
lionesslie <mixa_main@mail.ru>
2026-09-08 21:57:30 +0300

    Add files via upload

--- src/choise_install.cpp
+++ src/choise_install.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 void choise_install() {
     string pkg;
-    int choise_frmt;
+    string choise_frmt;
 
-    cout << "Pacman or Yay (0 / 1): ";
+    cout << "Pacman or Yay: ";
     cin >> choise_frmt;
 
-    if (choise_frmt == 0) {
+    if (choise_frmt == "pacman" || choise_frmt == "Pacman") {
         cout << "Package Name: ";
         cin >> pkg;
 
         string command = ("sudo pacman -S " + pkg);
         system(command.c_str());
     }
-    else if (choise_frmt == 1) {
+    else if (choise_frmt == "yay" || choise_frmt == "Yay") {
         cout << "Package Name: ";
         cin >> pkg;
 
